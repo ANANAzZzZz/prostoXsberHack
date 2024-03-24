2d51b2ebafb414bf5ff20172665eeb6462a066e5
ANANAzZzZz <vladscooltrol@gmail.com>
2024-03-24 11:32:29 +0300

    add nav template

--- app/routes.py
+++ app/routes.py
@@ -123,8 +123,180 @@ def logout():
     return redirect("/")
 
 
-@app.route('/main')
-def main():
+@app.route('/navigation')
+def navigation():
+
+    """
+    Navigation(
+        listOf(
+            Screen(
+                LazyColumn(
+                    listOf(
+                        Column(
+                            buttons = listOf(
+                                Button(
+                                    "Начать",
+                                    "",
+                                    "login",
+                                    TypeButton.Navigate.name,
+                                    1
+                                )
+                            ),
+                            texts = listOf(),
+                            edits = listOf(),
+                            images = listOf(Image("https://i.ibb.co/vHF7c5J/LOGO11.png", 0))
+                        )
+                    ),
+                ),
+                "Splash",
+                "splash"
+            ),
+            Screen(
+                LazyColumn(
+                    listOf(
+                        Column(
+                            edits = listOf(
+                                EditTextField(
+                                    "",
+                                    "username",
+                                    "Логин",
+                                    0
+                                ),
+                                EditTextField(
+                                    "",
+                                    "password",
+                                    "Пароль",
+                                    1
+                                )
+                            ),
+                            texts = listOf(),
+                            buttons = listOf(
+                                Button(
+                                    "Вход",
+                                    "login",
+                                    "main",
+                                    TypeButton.Create.name + "/" + TypeButton.Navigate.name,
+                                    2
+                                ),
+                                Button(
+                                    "Зарегестрироваться",
+                                    "",
+                                    "authorization",
+                                    TypeButton.Navigate.name,
+                                    3
+                                )
+                            ),
+                            images = listOf()
+                        )
+                    ),
+                ),
+                "Вход",
+                "login"
+            ),
+            Screen(
+                LazyColumn(
+                    listOf(
+                        Column(
+                            edits = listOf(
+                                EditTextField(
+                                    "",
+                                    "username",
+                                    "Логин",
+                                    0
+                                ),
+                                EditTextField(
+                                    "",
+                                    "password",
+                                    "Пароль",
+                                    1
+                                ),
+                                EditTextField(
+                                    "",
+                                    "name",
+                                    "Имя",
+                                    2
+                                ),
+                                EditTextField(
+                                    "",
+                                    "lastname",
+                                    "Фамилия",
+                                    3
+                                )
+                            ),
+                            texts = listOf(),
+                            buttons = listOf(
+                                Button(
+                                    "Зарегестрироваться",
+                                    "register",
+                                    "main",
+                                    TypeButton.Create.name + "/" + TypeButton.Navigate.name,
+                                    4,
+                                )
+                            ),
+                            images = listOf()
+                        )
+                    ),
+                ),
+                "Авторизация",
+                "authorization"
+            ),
+            Screen(
+                LazyColumn(
+                    listOf(
+                        Card(
+                            texts = listOf(
+                                TextField(
+                                    "Привет," + "user's name + lastname",
+                                    0
+                                )
+                            ),
+                            route = "",
+                            type = "",
+                            routeNavigation = ""
+                        ),
+
+                        LazyColumn(
+                            listOf(
+                                TextField("Треки", 0)
+                            )
+                        )
+                    ),
+                ),
+                "Главный экран",
+                "main"
+            ),
+            Screen(
+                LazyColumn(
+                    listOf(
+
+                    ),
+                ),
+                "Test1",
+                "trek/{id}",
+            ),
+            Screen(
+                LazyColumn(
+                    listOf(
+
+                    ),
+                ),
+                "Test1",
+                "test"
+            ),
+            Screen(
+                LazyColumn(
+                    listOf(
+
+                    ),
+                ),
+                "Test1",
+                "test1"
+            )
+        ),
+        "splash"
+    )
+    """
+
     return "we logged in"
 
 
